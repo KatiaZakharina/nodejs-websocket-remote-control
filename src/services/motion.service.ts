eb18745d635f2b991063558ab3f6e114a606a76f
Ekaterina Zaharina <katia.zakharina@gmail.com>
2023-01-22 19:42:28 +0300

    feat: add figure drawing

--- src/services/motion.service.ts
+++ src/services/motion.service.ts
@@ -14,16 +14,47 @@ const moveMouse = async (x: number, y: number) => {
   });
 };
 
-const drawCircle = async (radius: number) => {};
+const drawCircle = async (radius: number) => {
+  const { x: currentX, y: currentY } = await getMousePosition();
+  const offset = 3;
 
-const drawRectangle = async (width: number, height: number = width) => {};
+  const path = Array(360 / offset)
+    .fill(0)
+    .map((_, i) => {
+      const angle = i * offset;
+      const pointX = Math.cos((angle * Math.PI) / 180) * radius;
+      const pointY = Math.sin((angle * Math.PI) / 180) * radius;
+
+      return {
+        x: currentX - radius + pointX,
+        y: currentY - pointY,
+      };
+    });
+  path.push(path[0]);
+
+  mouse.config.mouseSpeed = 80;
+  mouse.drag(path);
+};
+
+const drawRectangle = async (width: number, height: number) => {
+  const { x: currentX, y: currentY } = await getMousePosition();
+
+  mouse.config.mouseSpeed = 30;
+  mouse.drag([
+    { x: currentX, y: currentY },
+    { x: currentX + width, y: currentY },
+    { x: currentX + width, y: currentY + height },
+    { x: currentX, y: currentY + height },
+    { x: currentX, y: currentY },
+  ]);
+};
 
 const printScreen = async () => {
   const size = 200;
   const { x, y } = await getMousePosition();
-  const region = new Region(x, y, size, size);
+  const capturingRegion = new Region(x, y, size, size);
 
-  const screenshot = await screen.grabRegion(region);
+  const screenshot = await screen.grabRegion(capturingRegion);
   const corrected = await screenshot.toRGB();
 
   const image = new Jimp(corrected);
